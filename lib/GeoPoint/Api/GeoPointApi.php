3ed1bd45b483dccda81f5e44ffeb4727356c6335
David Durost <david.durost@gmail.com>
2013-04-29 19:25:42 -0400

    fixed url construction

--- lib/GeoPoint/Api/GeoPointApi.php
+++ lib/GeoPoint/Api/GeoPointApi.php
@@ -19,7 +19,7 @@ class GeoPointApi extends Client
     {
         $this->client = $client instanceof Client ? $client : new Client();
 
-        $this->client->setUrl( 'api.neustar.biz/ipi/std/:path' );
+        $this->client->setUrl( ':protocolapi.neustar.biz/ipi/std/:path' );
         $this->client->setOption( 'protocol', 'http' );                       
     }    
     
